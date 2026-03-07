64a9af68e8d17681163ae6280194e70b9baf0486
suhardpa-pnj <suhard.pa@gmail.com>
2026-03-08 00:13:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // <-- On enlève l'extension .tsx ici
+import App from './app' // <-- "app" en minuscule ici pour correspondre à app.tsx
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
