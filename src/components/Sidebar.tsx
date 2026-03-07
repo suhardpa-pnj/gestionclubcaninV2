a27202b54a7cafd71e66f51c1554e3999e9c7b76
suhardpa-pnj <suhard.pa@gmail.com>
2026-03-07 22:04:43 +0100

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -23,6 +23,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeTab, setActiveTab }) => {
     { id: 'meute', label: 'Meute du Club', icon: <Dog size={20} /> },
     { id: 'boutique', label: 'Boutique', icon: <ShoppingCart size={20} /> },
     { id: 'evenements', label: 'Événements', icon: <CalendarDays size={20} /> },
+    { id: 'finances', label: 'Trésorerie', icon: <Landmark size={20} /> },
   ];
 
   return (
