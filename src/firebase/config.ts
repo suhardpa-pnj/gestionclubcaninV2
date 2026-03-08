1c1712f9fee2846d1c0459d2bab498aea02b7514
suhardpa-pnj <suhard.pa@gmail.com>
2026-03-08 10:35:38 +0100

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Ces informations se trouvent dans la console Firebase (Paramètres du projet > Général)
+// Tes clés récupérées depuis la console Firebase
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET_ID",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyDpnZMqv6NWmqbrfy17aZ7dHQnPqPq6Q5Ng",
+  authDomain: "gestionclubcaninv2.firebaseapp.com",
+  projectId: "gestionclubcaninv2",
+  storageBucket: "gestionclubcaninv2.firebasestorage.app",
+  messagingSenderId: "897167468441",
+  appId: "1:897167468441:web:a8f5f0f9e29155f4db5508",
+  measurementId: "G-2LV96K7KNH"
 };
 
 // Initialisation de Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialisation de la base de données Firestore
+// Initialisation de la base de données (Firestore)
 export const db = getFirestore(app);
