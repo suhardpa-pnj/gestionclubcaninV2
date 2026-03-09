5f94d9e635031612cd01ce08169e20d17c90b252
suhardpa-pnj <suhard.pa@gmail.com>
2026-03-09 18:46:45 +0100

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,12 +5,14 @@ import { getStorage } from "firebase/storage";
 // Tes clés récupérées depuis la console Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDpnZMqv6NWmqbrfy17aZ7dHQnPqPq6Q5Ng",
-  authDomain: "gestionclubcaninv2.firebaseapp.com",
+authDomain: "gestionclubcaninv2.firebaseapp.com",
   projectId: "gestionclubcaninv2",
-storageBucket: "gestion-acv-storage",
+  
+  // ICI : On force l'application à utiliser ton bucket GCS
+  storageBucket: "gestion-acv-storage", 
+  
   messagingSenderId: "897167468441",
-  appId: "1:897167468441:web:a8f5f0f9e29155f4db5508",
-  measurementId: "G-2LV96K7KNH"
+  appId: "1:897167468441:web:2f8b8f3b7b8f3b7b8f3b7b" // Ton App ID actuel
 };
 
 const app = initializeApp(firebaseConfig);
