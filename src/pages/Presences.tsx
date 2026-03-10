f3235998c842fbe9cf0e2e7233269378908c089b
suhardpa-pnj <suhard.pa@gmail.com>
2026-03-10 16:10:10 +0100

    Create Presences.tsx

---
+++ src/pages/Presences.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Organigramme = () => (
+  <div className="p-8 animate-in fade-in duration-700">
+    <h2 className="text-5xl font-serif italic text-[#1B4332]">L'Organigramme</h2>
+    <p className="text-[#BC6C25] text-[10px] font-black uppercase tracking-[0.3em] mt-3 italic">Administration • ACV</p>
+  </div>
+);
+export default Organigramme;
