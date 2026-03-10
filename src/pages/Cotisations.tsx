07a1512d7e48bcb361004653cfb21d21695cee7a
suhardpa-pnj <suhard.pa@gmail.com>
2026-03-10 21:53:08 +0100

    Delete src/pages/Cotisations.tsx

--- src/pages/Cotisations.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const Cotisations = () => (
-  <div className="p-8 animate-in fade-in duration-700">
-    <h2 className="text-5xl font-serif italic text-[#1B4332]">Cotisations</h2>
-    <p className="text-[#BC6C25] text-[10px] font-black uppercase tracking-[0.3em] mt-3 italic">Administration • ACV</p>
-  </div>
-);
-export default Cotisations;
