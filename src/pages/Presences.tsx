a21e73956ad54c72e6ebdb5a5194375e89504caf
suhardpa-pnj <suhard.pa@gmail.com>
2026-03-10 16:49:34 +0100

    Update Presences.tsx

--- src/pages/Presences.tsx
+++ src/pages/Presences.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-const Organigramme = () => (
+const Presences = () => (
   <div className="p-8 animate-in fade-in duration-700">
-    <h2 className="text-5xl font-serif italic text-[#1B4332]">L'Organigramme</h2>
+    <h2 className="text-5xl font-serif italic text-[#1B4332]">Presences</h2>
     <p className="text-[#BC6C25] text-[10px] font-black uppercase tracking-[0.3em] mt-3 italic">Administration • ACV</p>
   </div>
 );
-export default Organigramme;
+export default Presences;
